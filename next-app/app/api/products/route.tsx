1819aee0829a850d2e8385a04318ae8f601ca10c
Tikous <1120531060@qq.com>
2024-06-08 18:16:02 +0800

    Building products api

--- next-app/app/api/products/route.tsx
+++ next-app/app/api/products/route.tsx
@@ -2,21 +2,23 @@ import { NextRequest, NextResponse } from "next/server";
 import schema from "./schema";
 
 export function GET(request: NextRequest) {
-  // fetch users from a db
   return NextResponse.json([
-    { id: 1, name: "Jack" },
-    { id: 2, name: "Mick" },
+    { id: 1, name: "Milk", price: 2.5 },
+    { id: 2, name: "Bread", price: 3.5 },
   ]);
 }
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
-  // Validate
-  // If invalid, return 400
-  // Else, return
   const validation = schema.safeParse(body);
   if (!validation.success)
     return NextResponse.json(validation.error.errors, { status: 400 });
-  // 201 means server had created a new resourse
-  return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
+  /**
+   * Using ...body, may not safe, malicious user can send additional properties,
+   * that we are not going to accept
+   *  */
+  return NextResponse.json(
+    { id: 10, name: body.name, price: body.price },
+    { status: 201 }
+  );
 }
