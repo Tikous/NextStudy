d17f64a21af65c409a4fa766d4e50686f2554aee
Tikous <1120531060@qq.com>
2024-06-09 14:53:46 +0800

    Getting data

--- next-app/app/api/users/route.tsx
+++ next-app/app/api/users/route.tsx
@@ -1,12 +1,12 @@
 import { NextRequest, NextResponse } from "next/server";
 import schema from "./schema";
+import prisma from "@/prisma/client";
 
-export function GET(request: NextRequest) {
+export async function GET(request: NextRequest) {
+  // Get all users
   // fetch users from a db
-  return NextResponse.json([
-    { id: 1, name: "Jack" },
-    { id: 2, name: "Mick" },
-  ]);
+  const users = await prisma.user.findMany();
+  return NextResponse.json(users);
 }
 
 export async function POST(request: NextRequest) {
