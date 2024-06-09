10e7ed9176548be437099ed22d4297208f6b053d
Tikous <1120531060@qq.com>
2024-06-09 15:11:47 +0800

    Updating users

--- next-app/app/api/users/[id]/route.tsx
+++ next-app/app/api/users/[id]/route.tsx
@@ -19,7 +19,7 @@ export async function GET(
 
 export async function PUT(
   request: NextRequest,
-  { params }: { params: { id: number } }
+  { params }: { params: { id: string } }
 ) {
   // Validate the request body
   const body = await request.json();
@@ -30,12 +30,25 @@ export async function PUT(
   if (!validation.success)
     // If invalid return 400
     return NextResponse.json(validation.error.errors, { status: 400 });
-  if (params.id > 10)
+
+  const user = await prisma.user.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+
+  if (!user)
     // If doesn't exist return 404
     return NextResponse.json({ error: "User not found" }, { status: 404 });
+  
+  const updatedUser = await prisma.user.update({
+    where: { id: user.id },
+    data: {
+      name: body.name,
+      email: body.email
+    }
+  })
   // Update the user
   // Return the updated user
-  return NextResponse.json({ id: 1, name: body.name });
+  return NextResponse.json(updatedUser);
 }
 
 export function DELETE(
