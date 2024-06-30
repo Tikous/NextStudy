21696d0e073b0e3c773f038e0b973dafc5adf162
Tikous <1120531060@qq.com>
2024-06-30 15:07:40 +0800

    Search engine optimization

--- next-app/app/page.tsx
+++ next-app/app/page.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import ProductCard from "./components/ProductCard";
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
+import { Metadata } from "next";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
@@ -35,3 +36,12 @@ export default async function Home() {
   //   </main>
   // );
 }
+
+// export async function generateMetadata(): Promise<Metadata> {
+//   const product = await fetch("");
+
+//   return {
+//     title: "product.title",
+//     description: "...",
+//   };
+// }
