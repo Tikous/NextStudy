4f71be3ac4f5eccfafca02a6e3f8a0af3db76aed
Tikous <1120531060@qq.com>
2024-06-30 16:28:40 +0800

    upload

--- next-app/prisma/migrations/20240615114850_fix_password/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `hashedPasssword` on the `users` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `users` DROP COLUMN `hashedPasssword`,
-    ADD COLUMN `hashedPassword` VARCHAR(191) NULL;
