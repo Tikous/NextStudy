ae06876d656fa598c38249a0b40289b4d6b63cb6
Tikous <1120531060@qq.com>
2024-06-30 14:59:46 +0800

    Using fonts

--- next-app/app/layout.tsx
+++ next-app/app/layout.tsx
@@ -1,11 +1,16 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Roboto } from "next/font/google";
 import NavBar from "./NavBar";
 import { Suspense } from "react";
 import AuthProvider from "./auth/Provider";
 
 const inter = Inter({ subsets: ["latin"] });
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["400", "500"],
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -19,7 +24,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" data-theme="winter">
-      <body className={inter.className}>
+      <body className={roboto.className}>
         <AuthProvider>
           <NavBar />
           <main className="p-5">
