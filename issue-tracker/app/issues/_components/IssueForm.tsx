dfa2e8795c0750a6e5d9afdbaefb876e2c27a1a7
Tikous <1120531060@qq.com>
2024-07-27 17:47:43 +0800

    Update issues

--- issue-tracker/app/issues/_components/IssueForm.tsx
+++ issue-tracker/app/issues/_components/IssueForm.tsx
@@ -36,8 +36,9 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
   const onSubmit = handleSubmit(async (data) => {
     try {
       setSubmitting(true);
-      await axios.post("/api/issues", data);
-      router.replace("/issues");
+      if (issue) await axios.patch("/api/issues/" + issue.id, data);
+      else await axios.post("/api/issues", data);
+      router.push("/issues");
     } catch (error) {
       setSubmitting(false);
       setError("An unexpected error occurred.");
@@ -70,7 +71,7 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
         />
         <ErrorMessage>{errors.description?.message}</ErrorMessage>
         <Button disabled={isSubmitting}>
-          Submit New Issue
+          {issue ? "Update Issue" : "Submit New Issue"}{" "}
           {isSubmitting && <Spinner />}
         </Button>
       </form>
